338e309f78fc92d2cf3838a509ab99e036a3b4be
CowboyGH <kocergindenis81@gmail.com>
2024-03-07 18:06:31 +0700

    delete mars_photos_app.dart

--- task1_app/lib/main.dart
+++ task1_app/lib/main.dart
@@ -1,6 +1,26 @@
 import 'package:flutter/material.dart';
-import 'package:task1_app/mars_photos_app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task1_app/features/bloc/cubit.dart';
+import 'package:task1_app/features/mars_photos/mars_photos.dart';
 
 void main() {
   runApp(const MarsPhotosApp());
+}
+
+class MarsPhotosApp extends StatelessWidget {
+  const MarsPhotosApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        useMaterial3: true,
+      ),
+      home: BlocProvider(
+        create: (context) => NasaCubit(),
+        child: const MarsPhotosScreen()
+      )
+    );
+  }
 }
