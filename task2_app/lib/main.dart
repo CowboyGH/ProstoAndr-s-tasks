5c5996ceffc55f3e8d6f80b069341498641ca6c6
CowboyGH <kocergindenis81@gmail.com>
2024-03-10 14:30:42 +0700

    put together to main.dart

--- task2_app/lib/main.dart
+++ task2_app/lib/main.dart
@@ -1,6 +1,25 @@
 import 'package:flutter/material.dart';
-import 'package:task2_app/currency_list_app.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:task2_app/features/currency_list/bloc/cubit.dart';
+import 'package:task2_app/features/navigation/view/navigation_screen.dart';
+import 'package:task2_app/theme/theme.dart';
 
 void main() {
   runApp(const CurrencyListApp());
-} 
+}
+
+class CurrencyListApp extends StatelessWidget {
+  const CurrencyListApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: mainTheme,
+      home: BlocProvider(
+        create: (context) => CurrencyCubit(),
+        child: const NavigationScreen(),
+      ),
+    );
+  }
+}
